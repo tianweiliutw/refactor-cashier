3c9fadc223ca978715552da148c077f62633453f
tianwei.liu <tianwei.liu@thoughtworks.com>
2020-07-16 17:32:03 +0800

    refactor: remove comments, extract methods

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -23,45 +23,38 @@ public class OrderReceipt {
     }
 
     private void printOrder(StringBuilder output) {
+        printHeader(output);
         printOrderInfo(output);
-        printLineItemsAndSubtotal(output);
+        double[] results = processLineItems(output);
+        printTotal(output, results[0], results[1]);
+    }
+
+    private void printHeader(StringBuilder output) {
+        output.append("======Printing Orders======\n");
     }
 
     private void printOrderInfo(StringBuilder output) {
-        // print headers
-        output.append("======Printing Orders======\n");
-
-        // print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
         if (order.getCustomer() != null) {
             output.append(order.getCustomer().getName());
             output.append(order.getCustomer().getAddress());
-//            output.append(order.getCustomerLoyaltyNumber());
         }
     }
 
-    private void printLineItemsAndSubtotal(StringBuilder output) {
-        // prints lineItems
+    private double[] processLineItems(StringBuilder output) {
         double totalSalesTax = 0d;
         double subTotal = 0d;
+
         for (LineItem lineItem : order.getLineItems()) {
 
             printLineItem(lineItem, output);
 
-            // calculate sales tax @ rate of 10%
             double salesTax = lineItem.getSalesTax();
             totalSalesTax += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
             subTotal += lineItem.getSubTotal();
         }
 
-        // prints the state tax
-        output.append("Sales Tax").append('\t').append(totalSalesTax);
-
-        // print total amount
-        output.append("Total Amount").append('\t').append(subTotal);
-    };
+        return new double[] { totalSalesTax, subTotal };
+    }
 
     private void printLineItem(LineItem lineItem, StringBuilder output) {
         output.append(lineItem.getDescription());
@@ -72,5 +65,10 @@ public class OrderReceipt {
         output.append('\t');
         output.append(lineItem.totalAmount());
         output.append('\n');
+    }
+
+    private void printTotal(StringBuilder output, double totalSalesTax, double subTotal) {
+        output.append("Sales Tax").append('\t').append(totalSalesTax);
+        output.append("Total Amount").append('\t').append(subTotal);
     }
 }
